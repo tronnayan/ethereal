b812c047caca6cd89f493be5c7c775efa7b2e5a3
Nayan Arora <nayanarora55@gmail.com>
2024-10-26 13:51:02 +0530

    minor

--- Ethereal/settings.py
+++ Ethereal/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
